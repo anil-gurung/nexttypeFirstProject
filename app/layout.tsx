28d0a7c71d26e0971a56bf52c49591d01faa9555
uvyort <125897999+uvyort@users.noreply.github.com>
2024-07-04 10:28:00 +0545

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Container from "@/components/global/Container";
+import { Provider } from "@radix-ui/react-toast";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Next Storefront",
+  description: "A nifty store built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Provider>
+          <Navbar />
+          <Container className="py-20">{children}</Container>
+        </Provider>
+      </body>
+    </html>
+  );
+}
